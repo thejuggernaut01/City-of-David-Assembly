44b67d209a8f2580a36bc1e6fceee081ca0989f8
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-07-02 15:34:31 +0100

    changes made to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/UI/Loader";
 
-// import RootRoute from "./pages/RootRoute";
+import RootRoute from "./pages/RootRoute";
 import BlogPage from "./pages/BlogPage";
 import Error from "./pages/Error";
 import SinglePost from "./pages/SinglePost";
 import BlogRoute from "./pages/BlogRoute";
 
 // const BlogPage = lazy(() => import("./pages/BlogPage"));
-const RootRoute = lazy(() => import("./pages/RootRoute"));
+// const RootRoute = lazy(() => import("./pages/RootRoute"));
 
 function App() {
   const router = createBrowserRouter([
